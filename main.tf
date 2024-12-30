85d8dd51511b5bf4c8f5512fc7b4edadb9166b55
Devair Junior <devair.mjunior@gmail.com>
2024-12-30 18:34:50 -0300

    Test Glue ETL

--- main.tf
+++ main.tf
@@ -1,9 +1,46 @@
-# Define o provedor AWS
 provider "aws" {
   region = "us-east-2"
 }
 
-# Criação de um bucket S3
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "aws-actions-pipeline"
+resource "aws_iam_role" "glue_role" {
+  name               = "GlueRole"
+  assume_role_policy = data.aws_iam_policy_document.glue_assume_role_policy.json
 }
+
+data "aws_iam_policy_document" "glue_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["glue.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_glue_job" "glue_job" {
+  name     = "my-glue-job"
+  role_arn = aws_iam_role.glue_role.arn
+
+  command {
+    script_location = "s3://${aws_s3_bucket.script_bucket.id}/main.py"
+    python_version  = "3"
+  }
+
+  default_arguments = {
+    "--job-language" = "python"
+    "--TempDir"      = "s3://${aws_s3_bucket.script_bucket.id}/temp/"
+  }
+
+  max_retries = 1
+}
+
+resource "aws_s3_bucket" "script_bucket" {
+  bucket = "my-glue-scripts-bucket"
+}
+
+resource "aws_s3_bucket_object" "script_object" {
+  bucket = aws_s3_bucket.script_bucket.id
+  key    = "main.py"
+  source = "${path.module}/app/main.py"
+}
+  
