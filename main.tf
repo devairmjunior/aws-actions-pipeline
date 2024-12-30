20434a80bf769dcfe40b55d3747e3f24873b3b82
Devair Junior <devair.mjunior@gmail.com>
2024-12-30 19:23:28 -0300

    adding policies

--- main.tf
+++ main.tf
@@ -17,6 +17,16 @@ data "aws_iam_policy_document" "glue_assume_role_policy" {
   }
 }
 
+resource "aws_iam_policy" "glue_s3_policy" {
+  name   = "GlueS3AccessPolicy"
+  policy = file("${path.module}/s3_policy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "glue_s3_policy_attachment" {
+  role       = aws_iam_role.glue_role.name
+  policy_arn = aws_iam_policy.glue_s3_policy.arn
+}
+
 resource "aws_glue_job" "glue_job" {
   name     = "my-glue-job"
   role_arn = aws_iam_role.glue_role.arn
@@ -38,9 +48,10 @@ resource "aws_s3_bucket" "script_bucket" {
   bucket = "glue-scripts-bucket-devair"
 }
 
-resource "aws_s3_bucket_object" "script_object" {
+# Substituir aws_s3_bucket_object por aws_s3_object
+resource "aws_s3_object" "script_object" {
   bucket = aws_s3_bucket.script_bucket.id
   key    = "main.py"
   source = "${path.module}/app/main.py"
+  etag   = filemd5("${path.module}/app/main.py") # Adiciona checksum para garantir integridade
 }
-  
