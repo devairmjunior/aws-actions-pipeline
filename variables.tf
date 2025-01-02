1bd1ff0ee6dbe3fc8851299c24e040e6d2d70fea
Devair Junior <devair.mjunior@gmail.com>
2025-01-01 23:11:33 -0300

    fix

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "whatsapp_token" {
+  description = "Token do WhatsApp"
+  type        = string
+}
+
+variable "verify_token" {
+  description = "Token de verificação do webhook"
+  type        = string
+}
+
+variable "gemini_api_key" {
+  description = "Token de API do Gemini"
+  type        = string
+}
